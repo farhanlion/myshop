895996410f70b23d185f91db20cbc7ce447c6ab3
Farhan Ahsan <bbss1e3farhan@gmail.com>
2021-11-04 07:51:04 +0800

    asd

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module Myshop
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.autoloader = :classic
   end
 end
