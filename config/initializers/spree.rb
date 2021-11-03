bf61be5a8c3fe1752a6cece4c3fdcfd5dc790bf6
Farhan Ahsan <bbss1e3farhan@gmail.com>
2021-11-04 06:03:11 +0800

    a

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -35,7 +35,7 @@ Spree.config do |config|
   # Frontend:
 
   # Custom logo for the frontend
-  # config.logo = "logo/solidus.svg"
+  config.logo = "logo/trend.png"
 
   # Template to use when rendering layout
   # config.layout = "spree/layouts/spree_application"
@@ -44,7 +44,7 @@ Spree.config do |config|
   # Admin:
 
   # Custom logo for the admin
-  # config.admin_interface_logo = "logo/solidus.svg"
+  config.admin_interface_logo = "logo/trend.png"
 
   # Gateway credentials can be configured statically here and referenced from
   # the admin. They can also be fully configured from the admin.
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   # We ask that you don't use the :as option here, as Solidus relies on it being the default of "spree"
   mount Spree::Core::Engine, at: '/'
 
-mount SolidusPaypalCommercePlatform::Engine, at: '/solidus_paypal_commerce_platform'
+  mount SolidusPaypalCommercePlatform::Engine, at: '/solidus_paypal_commerce_platform'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
