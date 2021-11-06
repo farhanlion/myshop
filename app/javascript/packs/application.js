ee45f3f438422affd34fdc39a911cebd83beddf7
Farhan Ahsan <bbss1e3farhan@gmail.com>
2021-11-06 22:44:42 +0800

    ready to create front end

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,18 +3,19 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
+// import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-Rails.start()
+// Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
 import "bootstrap"
 import "../stylesheets/application"
 
+console.log('hello')
 document.addEventListener("turbolinks:load",()=>{
     $('[data-toggle="tooltip"]').tooltip()
     $('[data-toggle="tooltip"]').popover()
